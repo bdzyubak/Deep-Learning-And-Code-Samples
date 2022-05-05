8d376f6e296d863699ba24ea5aa6c8b281f82542
Bogdan Dzyubak <m047659@fod1.mayo.edu>
2022-05-05 10:55:54 -0500

    Updated module to prepare training data. Functional for image/mask file pairs in parallel folders. Image+csv label not supported at this time

--- shared_utils/prep_training_data.py
+++ shared_utils/prep_training_data.py
@@ -14,26 +14,51 @@ class ImgMaskDataset:
         # Some preprocessing tools are also defined. 
         self.data_path = data_path
         self.use_default_subdirs()
-        self.set_height_width((256,256))
-        self.set_batch_size(32)
+        self.set_image_dims_original(self.images[0])
+        self.set_batch_size(8) # This is the size to fetch data. Different from traning batch size set in the model init
 
 
     def use_default_subdirs(self): 
-        path = os.path.join(self.data_path,'images')
-        self.images = list_dir(path,target_type='files')
-        path = os.path.join(self.data_path,'masks')
-        self.masks = list_dir(path,target_type='files')
-    
-    def set_subdirs(self,path_images,path_masks): 
-        # Reset locations of images and masks folders if named in a non-standard way
-        self.images = list_dir(path_images,target_type='files')
-        self.masks = list_dir(path_masks,target_type='files')
+        path_images = os.path.join(self.data_path,'images')
+        path_masks = os.path.join(self.data_path,'masks')
+        self.set_subidrs(path_images,path_masks)
+        self.get_image_and_mask_list()
 
-    def set_height_width(self,dims:tuple): 
-        if min(dims) > 50 and max(dims) <10001: 
-            self.dims = dims
+    def set_subidrs(self,path_images:str,path_masks:str): 
+        self.path_images = path_images
+        self.path_masks = path_masks
+        self.get_image_and_mask_list()
+    
+    def get_image_and_mask_list(self): 
+        # Reset locations of images and masks folders if named in a non-standard way
+        self.images = list_dir(self.path_images,target_type='files')
+        self.masks = list_dir(self.path_masks,target_type='files')
+        if not self.images: 
+            raise(FileNotFoundError('Cannot find image files in: ' + self.path_images))
+        if not self.masks: 
+            raise(FileNotFoundError('Cannot find image files in: ' + self.path_masks))
+        if len(self.images) != len(self.masks): 
+            raise(FileNotFoundError('Mismatched number of image and mask files in: ' + os.path.dirname(self.path_images)))
+        
+    def set_image_target_dims(self,dims:tuple): 
+        if len(dims) != 3: 
+            raise(ValueError('Specify image dimensions as a tuple of (Height,Width,Channels).'))
+
+        if min(dims[0:2]) < 100 and max(dims[0:2]) >10000: 
+            print('Unexpected image size: ' + str(dims[0]) + 'x' + str(dims[1]))
+            self.image_dims_target = dims
+        self.image_dims_target = dims
+    
+    def set_image_dims_original(self,image_path): 
+        image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+        dims = image.shape
+        if len(dims) <2 or len(dims)>3: 
+            raise(ValueError('Unsupported image shape of ' + str(dims) + 'in ' + image_path))
+
+        if len(dims) == 2: 
+            self.image_dims_original = (dims[0],dims[1],1)
         else: 
-            raise(ValueError('Check image size: ' + str(dims[0]) + 'x' + str(dims[1])))
+            self.image_dims_original= dims
     
     def set_batch_size(self,batch_size): 
         self.batch_size = batch_size
@@ -41,15 +66,15 @@ class ImgMaskDataset:
     def prep_data_img_labels(self):
         batch_size = self.batch_size
         """ Dataset """
-        (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = load_data(self.images,self.masks)
+        (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = self.load_data(self.images,self.masks)
         train_x, train_y = shuffle(train_x, train_y)
 
         print(f"Train: {len(train_x)} - {len(train_y)}")
         print(f"Valid: {len(valid_x)} - {len(valid_y)}")
         print(f"Test: {len(test_x)} - {len(test_y)}")
 
-        self.train_dataset = tf_dataset(train_x, train_y, batch_size, self.dims)
-        self.valid_dataset = tf_dataset(valid_x, valid_y, batch_size, self.dims)
+        self.train_dataset = self.tf_dataset(train_x, train_y)
+        self.valid_dataset = self.tf_dataset(valid_x, valid_y)
 
         self.train_steps = (len(train_x)//batch_size)
         self.valid_steps = (len(valid_x)//batch_size)
@@ -60,58 +85,73 @@ class ImgMaskDataset:
         if len(valid_x) % batch_size != 0:
             self.valid_steps += 1
 
-def shuffling(x, y):
-    x, y = shuffle(x, y, random_state=42)
+    def shuffling(self,x, y):
+        x, y = shuffle(x, y, random_state=42)
 
-def read_image(path):
-    path = path.decode()
-    x = cv2.imread(path, cv2.IMREAD_COLOR)
-    x = cv2.resize(x, (W, H))
-    x = x/255.0
-    x = x.astype(np.float32)
-    return x
+    def read_image(self,path):
+        if not isinstance(path,str): 
+            path = path.decode()
+        x = cv2.imread(path, cv2.IMREAD_COLOR)
+        if self.image_dims_original != self.image_dims_target: 
+            x = cv2.resize(x, (self.dims[0], self.dims[1]))
+        x = x/255.0
+        x = x.astype(np.float32)
+        return x
 
-def read_mask(path):
-    path = path.decode()
-    x = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-    x = cv2.resize(x, (W, H))
-    x = x/255.0
-    x = x.astype(np.float32)
-    x = np.expand_dims(x, axis=-1)
-    return x
+    def read_mask(self,path):
+        path = path.decode()
+        x = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+        x = cv2.resize(x, (self.dims[0], self.dims[1]))
+        x = x/255.0
+        x = x.astype(np.float32)
+        x = np.expand_dims(x, axis=-1)
+        return x
 
-def tf_parse(x, y, dims):
-    def _parse(x, y):
-        x = read_image(x)
-        y = read_mask(y)
+    def tf_parse(self, x, y):
+        def _parse(x, y):
+            x = self.read_image(x)
+            y = self.read_mask(y)
+            return x, y
+
+        x, y = tf.numpy_function(_parse, [x, y], [tf.float32, tf.float32])
+        x.set_shape([self.image_dims_original[0], self.image_dims_original[1], 3])
+        y.set_shape([self.image_dims_original[0], self.image_dims_original[1], 1])
         return x, y
 
-    x, y = tf.numpy_function(_parse, [x, y], [tf.float32, tf.float32])
-    x.set_shape([dims[0], dims[1], 3])
-    y.set_shape([dims[0], dims[1], 1])
-    return x, y
+    def tf_dataset(self, train_x, train_y):
+        dataset = tf.data.Dataset.from_tensor_slices((train_x, train_y))
+        # Preprocess images and masks
+        dataset = dataset.map(self.tf_parse)
+        dataset = dataset.batch(self.batch_size)
+        dataset = dataset.prefetch(tf.data.AUTOTUNE)
+        return dataset
 
-def tf_dataset(train_x, train_y, batch=8,dims=(256,256)):
-    dataset = tf.data.Dataset.from_tensor_slices((train_x, train_y))
-    # Preprocess images and masks
-    dataset = dataset.map(lambda x, y: tf_parse(x,y,dims))
-    dataset = dataset.batch(batch)
-    dataset = dataset.prefetch(tf.data.AUTOTUNE)
-    return dataset
+    def load_data(self, images, masks, split=0.2):
+        size = int(len(images) * split)
 
-def load_data(images, masks, split=0.2):
-    size = int(len(images) * split)
+        # Split data into train and val sets
+        train_x, valid_x = train_test_split(images, test_size=size, random_state=42)
+        train_y, valid_y = train_test_split(masks, test_size=size, random_state=42)
 
-    # Split data into train and val sets
-    train_x, valid_x = train_test_split(images, test_size=size, random_state=42)
-    train_y, valid_y = train_test_split(masks, test_size=size, random_state=42)
+        # Further split off a test set from train
+        train_x, test_x = train_test_split(train_x, test_size=size, random_state=42)
+        train_y, test_y = train_test_split(train_y, test_size=size, random_state=42)
 
-    # Further split off a test set from train
-    train_x, test_x = train_test_split(train_x, test_size=size, random_state=42)
-    train_y, test_y = train_test_split(train_y, test_size=size, random_state=42)
+        return (train_x, train_y), (valid_x, valid_y), (test_x, test_y)
 
-    return (train_x, train_y), (valid_x, valid_y), (test_x, test_y)
+# class Augment(tf.keras.layers.Layer):
+#   # Use train_batches = (train_images.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat().map(Augment()).prefetch(buffer_size=tf.data.AUTOTUNE))
+#   # test_batches = test_images.batch(BATCH_SIZE)
+#   def __init__(self, seed=42):
+#     super().__init__()
+#     # both use the same seed, so they'll make the same random changes.
+#     self.augment_inputs = tf.keras.layers.RandomFlip(mode="horizontal", seed=seed)
+#     self.augment_labels = tf.keras.layers.RandomFlip(mode="horizontal", seed=seed)
 
+#   def call(self, inputs, labels):
+#     inputs = self.augment_inputs(inputs)
+#     labels = self.augment_labels(labels)
+#     return inputs, labels
 
 # if __name__ == "__main__":
 #     main()

92d351a4d7ec864d5f32a48231209279f8ade175
bdzyubak <illan7@gmail.com>
2022-08-20 15:27:02 -0500

    Updated gitignore and dependencies setup script.

--- utility_scripts/Windows/setup_python.bat
+++ utility_scripts/Windows/setup_python.bat
@@ -3,4 +3,12 @@ python -m pip install pydicom
 python -m pip install pandas
 python -m pip install numpy
 python -m pip install tensorflow
+python -m pip install tensorflow_datasets
+python -m pip install git+https://github.com/tensorflow/examples.git
+python -m pip install scikit-image
+python -m pip install opencv-python
+python -m pip install matplotlib
+python -m pip install IPython
+python -m pip install seaborn
+python -m pip install pydot
 pause
